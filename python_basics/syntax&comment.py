d842f56ee42ae7cda7f3b23819bef4c0c61ee617
Bishwarup Biswas <bishwarupbiswas07@gmail.com>
2025-09-07 23:14:01 +0600

    Delete python_basics/syntax&comment.py

--- python_basics/syntax&comment.py
+++
@@ -1,9 +0,0 @@
-#python Syntax:
-print("Hello,World!")
-
-#comments:
-# ----> This hash mark is used for single comments.
-"""
-Hey, now you are in my syntax&comment.py file.
-"""
-# ("""  & ''' This double & single quotes are used for multiple comments.)
